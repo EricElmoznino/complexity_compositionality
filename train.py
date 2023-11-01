64e54b2d08f9b06524feffba49f1a47b3615f297
Eric Elmoznino <eric.elmoznino@gmail.com>
2023-11-01 16:56:30 -0400

    - VQ-VAE pipeline is now finished and just needs to be tested. - Filled in trainer.py.

---
+++ train.py
@@ -0,0 +1,31 @@
+import hydra
+from lightning import Trainer, seed_everything
+
+
+@hydra.main(config_path="./configs/", config_name="train", version_base=None)
+def train(cfg):
+    seed_everything(cfg.seed)
+
+    framework = hydra.utils.instantiate(cfg.experiment.framework)
+    datamodule = hydra.utils.instantiate(cfg.experiment.data)
+    logger = hydra.utils.instantiate(cfg.logger) if cfg.logger else False
+    callbacks = (
+        hydra.utils.instantiate(cfg.experiment.callbacks)
+        if cfg.experiment.callbacks
+        else None
+    )
+
+    trainer = Trainer(
+        logger=logger,
+        callbacks=callbacks,
+        enable_checkpointing=False,
+        **cfg.experiment.trainer,
+    )
+    trainer.fit(model=framework, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    """Run with:
+    `python train.py experiment=vqvae/[experiment_name].yaml [overrides]`
+    """
+    train()
