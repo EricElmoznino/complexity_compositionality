5c97f01ad77e863a9737dda96220a49a0a3d975a
EricElmoznino <eric.elmoznino@gmail.com>
2024-09-18 13:20:36 -0400

    Changed real languages run config to train for longer on each chunk of data

--- scripts/real_languages_prequential.sh
+++ scripts/real_languages_prequential.sh
@@ -1,1 +1,8 @@
-python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/logs experiment=prequential/real_languages 
+python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/logs experiment=prequential/real_languages \
+    seed=0,1,2 \
+    experiment.data.data_dir=\
+/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/english/,\
+/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/french/,\
+/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/spanish/,\
+/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/german/,\
+/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/japanese/
