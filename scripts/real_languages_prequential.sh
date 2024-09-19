984173abc92f46e431d4876a96afcd2903c337f3
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-19 12:32:35 -0400

    Additional time for real language jobs

--- scripts/real_languages_prequential.sh
+++ scripts/real_languages_prequential.sh
@@ -1,4 +1,5 @@
 python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/logs experiment=prequential/real_languages \
+    hydra.launcher.timeout_min=2880 \
     seed=0,1,2 \
     experiment.data.data_dir=\
 /home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/english/,\
