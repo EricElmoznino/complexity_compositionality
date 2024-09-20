9f9da44c852aba2f6347e33a8db8e86fe586f8e2
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-19 23:07:54 -0400

    Updated folder paths

--- scripts/real_languages_prequential.sh
+++ scripts/real_languages_prequential.sh
@@ -2,8 +2,8 @@ python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.e
     hydra.launcher.timeout_min=2880 \
     seed=0,1,2 \
     experiment.data.data_dir=\
-/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/english/,\
-/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/french/,\
-/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/spanish/,\
-/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/german/,\
-/home/mila/e/eric.elmoznino/scratch/complexity_compositionality/data/real_languages/coco-captions/japanese/
+/network/scratch/e/eric.elmoznino/complexity_compositionality/data/real_languages/coco-captions/english/,\
+/network/scratch/e/eric.elmoznino/complexity_compositionality/data/real_languages/coco-captions/french/,\
+/network/scratch/e/eric.elmoznino/complexity_compositionality/data/real_languages/coco-captions/spanish/,\
+/network/scratch/e/eric.elmoznino/complexity_compositionality/data/real_languages/coco-captions/german/,\
+/network/scratch/e/eric.elmoznino/complexity_compositionality/data/real_languages/coco-captions/japanese/
